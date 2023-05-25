421a1fa90a416d3a8ab19dd2d0a2ad451c6d4e83
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-05-25 15:02:00 +0900

    :pencil2: タイプミスの修正#11

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Webapp
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.i18n.default_local = :ja
+    config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.time_zone = 'Tokyo'
