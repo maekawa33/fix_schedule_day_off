dca17e721f5d14f5824e0309c03c254c47d2ac1a
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-05-27 18:02:25 +0900

    :sparkles: ルーティングの追加#16

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resource :users, only: [:new, :create]
   get 'top', to: 'static_pages#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
