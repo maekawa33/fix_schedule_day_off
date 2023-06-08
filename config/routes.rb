6a3df2819c8cb18cc389f4a1dc3e7bd42d826eab
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-06-09 00:38:35 +0900

    :sparkles: ログイン関連のルートの追加、topページをrootに修正#18

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
-  get 'top', to: 'static_pages#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'static_pages#top'
 end
