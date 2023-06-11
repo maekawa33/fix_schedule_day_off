bde83d6cfe195448e5e1db537e38f23c2fe11be9
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-06-11 21:52:30 +0900

    :sparkles: ユーザー新規登録時にアバター画像を追加できる#22
    git status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  has_one_attached :avatar do |attachable|
+    attachable.variant :user_index, resize_to_limit: [100, 100]
+  end
   has_many :schedules, dependent: :destroy
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
