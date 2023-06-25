6cedbf27596bf35e966f344c1fb1e020727760cd
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-06-25 20:38:13 +0900

    :adhesive_bandage: Update時にも対応できるように修正#25

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  before_create :default_avatar
+  before_save :default_avatar
+
   authenticates_with_sorcery!
+
   has_one_attached :avatar do |attachable|
     attachable.variant :user_index, resize_to_limit: [100, 100]
   end
+
   has_many :schedules, dependent: :destroy
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
