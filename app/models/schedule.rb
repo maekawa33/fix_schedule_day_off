2dc06866d58c98e43d189db6513102099986f811
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-06-25 20:28:46 +0900

    :sparkles: Eventsとのリレーションを追加#25

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,7 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+  has_many :events, dependent: :destroy
+
   validates :schedule_title, presence: true, length: { maximum: 13 }
   validates :assumed_number_people, presence: true
   validates :get_up_time, presence: true
