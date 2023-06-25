bc7eb3a939470d46defdadbfe1c4711d36cd7613
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-06-25 20:48:16 +0900

    :sparkles: イベント関連のルーティングの追加#25

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
-  resources :schedules, only: %i[new create index]
+  resources :schedules do
+    resources :events
+  end
   root to: 'static_pages#top'
 end
