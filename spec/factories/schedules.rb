517a021e9c29f61f6a8a990e0e4ed9ca57554d12
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-06-25 21:05:38 +0900

    :adhesive_bandage: Scheduleファクトリの起床時間、就寝時間の修正#25

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :schedule do
     schedule_title { "スケジュール" }
     assumed_number_people { 1 }
-    get_up_time { "1970-01-01 05:22:24" }
-    sleep_time { "1970-01-01 05:22:24" }
+    get_up_time { "2000-01-01 07:00:00" }
+    sleep_time { "2000-01-01 21:00:00" }
     user
   end
 end
