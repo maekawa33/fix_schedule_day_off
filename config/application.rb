f6727a9c4b7952fa582b5c9fef62519159eb0f23
Maekawa Kouki <maekawa.kouki33@gmail.com>
2023-06-25 20:45:51 +0900

    :adhesive_bandage: Time_zoneをAsia/Tokyoに修正#25

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Webapp
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
 
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'Asia/Tokyo'
 
     config.generators do |g|
       g.helper false
